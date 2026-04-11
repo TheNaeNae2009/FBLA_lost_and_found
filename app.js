32cb194d84f92f38ea2fe2c639184adc5d09d52e
TheNaeNae2009 <TheNaeNae2009@gmail.com>
2026-04-11 08:17:06 -0500

    implemented arcjet

--- app.js
+++ app.js
@@ -12,14 +12,20 @@ const PORT = process.env.PORT || 3000;
 // Routers
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
+
+// DB
 import connectToDatabase from "./database/mongodb.js";
+
+// Middleware
 import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
+app.use(arcjetMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
