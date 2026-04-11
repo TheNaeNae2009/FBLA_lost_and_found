218ec0f17760188ab726de2a5d887b7c8f4b7a1a
TheNaeNae2009 <TheNaeNae2009@gmail.com>
2026-04-10 22:04:16 -0500

    added error middle ware and user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,1 +1,44 @@
 import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User Name is required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, "User Email is required"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      minLength: 5,
+      maxLength: 255,
+      match: [/\S+@\S+\.\S+/, "Please fill a valid email adress"],
+    },
+    email: {
+      type: String,
+      required: [true, "User Email is required"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      minLength: 5,
+      maxLength: 255,
+      match: [/\S+@\S+\.\S+/, "Please fill a valid email adress"],
+    },
+    password: {
+      type: String,
+      required: [true, "User Password is required"],
+      trim: true,
+      minLength: 6,
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
