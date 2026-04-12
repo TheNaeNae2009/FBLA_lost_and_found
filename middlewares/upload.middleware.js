ef9277105fb3b2ab87e2438d2669ff3a87df8451
TheNaeNae2009 <TheNaeNae2009@gmail.com>
2026-04-12 13:44:25 -0500

    test

--- middlewares/upload.middleware.js
+++ middlewares/upload.middleware.js
@@ -28,11 +28,4 @@ export const upload = multer({
   limits: {
     fileSize: 5 * 1024 * 1024, // 5MB max
   },
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype.startsWith("image/")) {
-      cb(null, true);
-    } else {
-      cb(new Error("Only images allowed"));
-    }
-  },
 });
