cf897743b171d64e3589fd615b082158bb50c4d6
TheNaeNae2009 <TheNaeNae2009@gmail.com>
2026-04-12 10:14:53 -0500

    created api calls

--- public/student-dashboard.js
+++ public/student-dashboard.js
@@ -9,7 +9,6 @@ const reportItemContainer = document.querySelector("#report-item-container");
 
 browseItemsButton.addEventListener("click", () => {
   browseItemsContainer.style.display = "flex";
-  browseItemsContainer.style.width = "100%";
   listingInfoContainer.style.display = "none";
   inquireContainer.style.display = "none";
   claimItemContainer.style.display = "none";
@@ -22,4 +21,25 @@ reportItemsButton.addEventListener("click", () => {
   inquireContainer.style.display = "none";
   claimItemContainer.style.display = "none";
   reportItemContainer.style.display = "flex";
+});
+
+const reportItemForm = document.querySelector("#new-item-form");
+
+reportItemForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = new formData(reportItemForm);
+
+  try {
+    const res = await ("https://mhslostandfound.com/api/v1/items/",
+    {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 });
